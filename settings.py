2af9e72ddcff8c888102fa805757da9fad59f3e5
Sean Fallmann <sfallmann@pbm.local>
2015-12-17 15:15:26 -0500

    FEAT:  Lesson 2 Changes
    Added the code to complete quizes for Lesson 2.

--- settings.py
+++ settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '699420069607-lc3u348lsgvoi9h5n8v8denq2171hsr9.apps.googleusercontent.com'
 
